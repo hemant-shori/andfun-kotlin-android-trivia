398bf5e24d530a0779b24c75e26a6e519403a584
Hemant Shori <hemantshori93@gmail.com>
2024-03-09 01:32:49 +0100

    Update Trivia app fragments to compose UI
    Re-written AboutFragment.kt, GameOverFragment.kt,
    GameWonFragment.kt using compose Ui.
    Also, Added original icons, string and dimens.xml.

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,9 +20,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
+import com.example.android.navigation.ui.theme.AndroidTriviaTheme
 
 
 class GameWonFragment : Fragment() {
@@ -30,17 +54,58 @@ class GameWonFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // TODO : rewrite with compose
         return ComposeView(requireContext()).apply {
             setContent {
-                MyComposeContent()
+                AndroidTriviaTheme {
+                    GameWonContent()
+                }
             }
         }
     }
 
     @Composable
-    fun MyComposeContent() {
-        // Your Compose UI code goes here
-        // TODO : rewrite fragment_game_won with compose
+    fun GameWonContent() {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = colorResource(id = R.color.youWinBackground)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+            ) {
+                val commonModifier = Modifier
+                    .padding(
+                        top = dimensionResource(id = R.dimen.vertical_margin),
+                        start = dimensionResource(id = R.dimen.horizontal_margin),
+                        end = dimensionResource(id = R.dimen.horizontal_margin)
+                    )
+                Image(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.you_win),
+                    contentDescription = "",
+                    modifier = commonModifier
+                        .fillMaxWidth()
+                        .height(dimensionResource(id = R.dimen.game_over_height))
+                )
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = commonModifier
+                        .align(alignment = Alignment.CenterHorizontally)
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.next_match),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = dimensionResource(id = R.dimen.button_text_size).value.sp,
+                    )
+                }
+            }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GameWonPreview() {
+        AndroidTriviaTheme {
+            GameWonContent()
+        }
     }
 }
